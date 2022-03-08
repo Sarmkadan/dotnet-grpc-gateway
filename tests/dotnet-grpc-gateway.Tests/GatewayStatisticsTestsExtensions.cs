38623a1afbe7dd823fa75a1d8e7e76c53a706f4c
Vladyslav Zaiets <rutova2@gmail.com>
2022-03-08 07:40:19 +0200

    add extension helpers for GatewayStatistics.

---
+++ tests/dotnet-grpc-gateway.Tests/GatewayStatisticsTestsExtensions.cs
@@ -0,0 +1,143 @@
+#nullable enable
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =====================================================================
+
+using DotNetGrpcGateway.Domain;
+
+namespace DotNetGrpcGateway.Tests;
+
+/// <summary>
+/// Extension methods for GatewayStatisticsTests to provide additional utility functionality
+/// </summary>
+public static class GatewayStatisticsTestsExtensions
+{
+    /// <summary>
+    /// Creates a GatewayStatistics instance with realistic production-like values for testing
+    /// </summary>
+    /// <param name="totalRequests">Total requests processed</param>
+    /// <param name="successRate">Success rate percentage (0-100)</param>
+    /// <param name="avgResponseTimeMs">Average response time in milliseconds</param>
+    /// <param name="dataProcessedBytes">Total data processed in bytes</param>
+    /// <returns>Configured GatewayStatistics instance</returns>
+    public static GatewayStatistics CreateTestStatistics(
+        this GatewayStatisticsTests _,
+        long totalRequests = 1000,
+        double successRate = 95.5,
+        double avgResponseTimeMs = 125.5,
+        long dataProcessedBytes = 1024 * 1024)
+    {
+        var stats = new GatewayStatistics
+        {
+            TotalRequestsProcessed = totalRequests,
+            SuccessfulRequests = (long)(totalRequests * successRate / 100),
+            FailedRequests = (long)(totalRequests * (100 - successRate) / 100),
+            SuccessRate = successRate,
+            AverageResponseTimeMs = avgResponseTimeMs,
+            MinResponseTimeMs = avgResponseTimeMs * 0.5,
+            MaxResponseTimeMs = avgResponseTimeMs * 2,
+            TotalDataProcessedBytes = dataProcessedBytes,
+            ActiveConnections = 15,
+            PeakConnections = 30,
+            StatisticsDate = DateTime.UtcNow.Date.AddDays(-1)
+        };
+
+        // Add some service and method tracking
+        stats.RecordServiceRequest("UserService");
+        stats.RecordServiceRequest("UserService");
+        stats.RecordServiceRequest("OrderService");
+
+        stats.RecordMethodCall("GetUser");
+        stats.RecordMethodCall("GetUser");
+        stats.RecordMethodCall("CreateOrder");
+
+        stats.RecordError("TimeoutError");
+        stats.RecordError("ConnectionError");
+
+        stats.RecordCacheHit(isHit: true);
+        stats.RecordCacheHit(isHit: true);
+        stats.RecordCacheHit(isHit: false);
+
+        stats.UpdateServiceHealth(healthyCount: 8, unhealthyCount: 2);
+
+        return stats;
+    }
+
+    /// <summary>
+    /// Creates a GatewayStatistics instance with minimal default values
+    /// </summary>
+    /// <param name="date">Statistics date (defaults to today)</param>
+    /// <returns>GatewayStatistics instance with default values</returns>
+    public static GatewayStatistics CreateEmptyStatistics(
+        this GatewayStatisticsTests _,
+        DateTime? date = null)
+    {
+        return new GatewayStatistics
+        {
+            StatisticsDate = date ?? DateTime.UtcNow.Date
+        };
+    }
+
+    /// <summary>
+    /// Creates a GatewayStatistics instance with error conditions for negative testing
+    /// </summary>
+    /// <param name="negativeTotalRequests">Whether to set negative total requests</param>
+    /// <param name="negativeSuccessRate">Whether to set success rate below zero</param>
+    /// <param name="invalidMinMax">Whether to set min > max response times</param>
+    /// <returns>GatewayStatistics instance configured for error scenarios</returns>
+    public static GatewayStatistics CreateErrorStatistics(
+        this GatewayStatisticsTests _,
+        bool negativeTotalRequests = false,
+        bool negativeSuccessRate = false,
+        bool invalidMinMax = false)
+    {
+        var stats = new GatewayStatistics
+        {
+            TotalRequestsProcessed = negativeTotalRequests ? -100 : 500,
+            SuccessfulRequests = 450,
+            FailedRequests = 50,
+            SuccessRate = negativeSuccessRate ? -5.0 : 90.0,
+            AverageResponseTimeMs = 100.0,
+            MinResponseTimeMs = invalidMinMax ? 200.0 : 50.0,
+            MaxResponseTimeMs = invalidMinMax ? 100.0 : 150.0,
+            TotalDataProcessedBytes = 512 * 1024,
+            StatisticsDate = DateTime.UtcNow.Date
+        };
+
+        return stats;
+    }
+
+    /// <summary>
+    /// Creates a GatewayStatistics instance with zero values for edge case testing
+    /// </summary>
+    /// <param name="zeroRequests">Whether to set all request counters to zero</param>
+    /// <returns>GatewayStatistics instance with zero values</returns>
+    public static GatewayStatistics CreateZeroStatistics(
+        this GatewayStatisticsTests _,
+        bool zeroRequests = true)
+    {
+        var stats = new GatewayStatistics
+        {
+            StatisticsDate = DateTime.UtcNow.Date,
+            TotalRequestsProcessed = zeroRequests ? 0 : 100,
+            SuccessfulRequests = 0,
+            FailedRequests = 0,
+            SuccessRate = 0.0,
+            AverageResponseTimeMs = 0.0,
+            MinResponseTimeMs = 0.0,
+            MaxResponseTimeMs = 0.0,
+            TotalDataProcessedBytes = 0,
+            ActiveConnections = 0,
+            PeakConnections = 0,
+            CacheHitRate = 0.0,
+            CacheHits = 0,
+            CacheMisses = 0,
+            HealthyServices = 0,
+            UnhealthyServices = 0,
+            TotalServices = 0
+        };
+
+        return stats;
+    }
+}
