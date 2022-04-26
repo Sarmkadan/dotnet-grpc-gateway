dc146d2e74fb181a127492f65bab16f3bc6011b2
Vladyslav Zaiets <rutova2@gmail.com>
2022-04-26 14:18:02 +0300

    Cleanup

--- src/dotnet-grpc-gateway/Domain/RequestMetricJsonExtensions.cs
+++ src/dotnet-grpc-gateway/Domain/RequestMetricJsonExtensions.cs
@@ -2,7 +2,7 @@
 // =============================================================================
 // Author: Vladyslav Zaiets | https://sarmkadan.com
 // CTO & Software Architect
-// =============================================================================
+// =====================================================================
 
 using System.Text.Json;
 using System.Text.Json.Serialization;
@@ -10,11 +10,11 @@ using System.Text.Json.Serialization;
 namespace DotNetGrpcGateway.Domain;
 
 /// <summary>
-/// Provides System.Text.Json serialization extensions for RequestMetric
+/// Provides System.Text.Json serialization extensions for <see cref="RequestMetric"/>.
 /// </summary>
 public static class RequestMetricJsonExtensions
 {
-    private static readonly JsonSerializerOptions _jsonOptions = new JsonSerializerOptions
+    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
     {
         PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
         WriteIndented = false,
@@ -23,17 +23,18 @@ public static class RequestMetricJsonExtensions
     };
 
     /// <summary>
-    /// Serializes the RequestMetric to a JSON string
+    /// Serializes the <see cref="RequestMetric"/> to a JSON string.
     /// </summary>
-    /// <param name="value">The RequestMetric to serialize</param>
-    /// <param name="indented">Whether to format the JSON with indentation</param>
-    /// <returns>JSON string representation of the RequestMetric</returns>
+    /// <param name="value">The <see cref="RequestMetric"/> to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation.</param>
+    /// <returns>JSON string representation of the <see cref="RequestMetric"/>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
     public static string ToJson(this RequestMetric value, bool indented = false)
     {
-        if (value == null)
-        {
-            throw new ArgumentNullException(nameof(value));
-        }
+        ArgumentNullException.ThrowIfNull(value);
+        ArgumentException.ThrowIfNullOrWhiteSpace(value.ServiceName);
+        ArgumentException.ThrowIfNullOrWhiteSpace(value.MethodName);
+        ArgumentException.ThrowIfNullOrWhiteSpace(value.ClientIpAddress);
 
         var options = indented
             ? new JsonSerializerOptions(_jsonOptions)
@@ -46,26 +47,23 @@ public static class RequestMetricJsonExtensions
     }
 
     /// <summary>
-    /// Deserializes a RequestMetric from a JSON string
+    /// Deserializes a <see cref="RequestMetric"/> from a JSON string.
     /// </summary>
-    /// <param name="json">JSON string to deserialize</param>
-    /// <returns>The deserialized RequestMetric, or null if JSON is null or empty</returns>
+    /// <param name="json">JSON string to deserialize. Can be <see langword="null"/> or whitespace.</param>
+    /// <returns>The deserialized <see cref="RequestMetric"/>, or <see langword="null"/> if <paramref name="json"/> is <see langword="null"/> or empty.</returns>
     public static RequestMetric? FromJson(string json)
     {
-        if (string.IsNullOrWhiteSpace(json))
-        {
-            return null;
-        }
-
-        return JsonSerializer.Deserialize<RequestMetric>(json, _jsonOptions);
+        return string.IsNullOrWhiteSpace(json)
+            ? null
+            : JsonSerializer.Deserialize<RequestMetric>(json, _jsonOptions);
     }
 
     /// <summary>
-    /// Attempts to deserialize a RequestMetric from a JSON string
+    /// Attempts to deserialize a <see cref="RequestMetric"/> from a JSON string.
     /// </summary>
-    /// <param name="json">JSON string to deserialize</param>
-    /// <param name="value">Output parameter for the deserialized RequestMetric</param>
-    /// <returns>True if deserialization succeeded, false otherwise</returns>
+    /// <param name="json">JSON string to deserialize. Can be <see langword="null"/> or whitespace.</param>
+    /// <param name="value">Output parameter for the deserialized <see cref="RequestMetric"/>.</param>
+    /// <returns><see langword="true"/> if deserialization succeeded; otherwise, <see langword="false"/>.</returns>
     public static bool TryFromJson(string json, out RequestMetric? value)
     {
         value = null;
