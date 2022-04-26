d5deb2a0eabf9c959a5fa77a68907ba1e9c4c6d8
Vladyslav Zaiets <rutova2@gmail.com>
2022-04-26 12:40:11 +0300

    tidy up GrpcServiceJson json handling.

--- src/dotnet-grpc-gateway/Domain/GrpcServiceJsonExtensions.cs
+++ src/dotnet-grpc-gateway/Domain/GrpcServiceJsonExtensions.cs
@@ -6,7 +6,7 @@ using System.Text.Json.Serialization;
 namespace DotNetGrpcGateway.Domain;
 
 /// <summary>
-/// Provides System.Text.Json serialization extensions for GrpcService
+/// Provides System.Text.Json serialization extensions for <see cref="GrpcService"/>.
 /// </summary>
 public static class GrpcServiceJsonExtensions
 {
@@ -19,33 +19,29 @@ public static class GrpcServiceJsonExtensions
     };
 
     /// <summary>
-    /// Serializes a GrpcService instance to a JSON string
+    /// Serializes a <see cref="GrpcService"/> instance to a JSON string.
     /// </summary>
-    /// <param name="value">The GrpcService instance to serialize</param>
-    /// <param name="indented">Whether to format the JSON with indentation</param>
-    /// <returns>A JSON string representation of the GrpcService</returns>
+    /// <param name="value">The <see cref="GrpcService"/> instance to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation.</param>
+    /// <returns>A JSON string representation of the <see cref="GrpcService"/>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
     public static string ToJson(this GrpcService value, bool indented = false)
     {
-        if (value == null)
-        {
-            throw new ArgumentNullException(nameof(value));
-        }
+        ArgumentNullException.ThrowIfNull(value);
 
         var options = indented
             ? new JsonSerializerOptions(_jsonSerializerOptions)
-            {
-                WriteIndented = true
-            }
+            { WriteIndented = true }
             : _jsonSerializerOptions;
 
         return JsonSerializer.Serialize(value, options);
     }
 
     /// <summary>
-    /// Deserializes a JSON string to a GrpcService instance
+    /// Deserializes a JSON string to a <see cref="GrpcService"/> instance.
     /// </summary>
-    /// <param name="json">The JSON string to deserialize</param>
-    /// <returns>A GrpcService instance, or null if JSON is null or empty</returns>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>A <see cref="GrpcService"/> instance, or null if JSON is null or empty.</returns>
     public static GrpcService? FromJson(string json)
     {
         if (string.IsNullOrWhiteSpace(json))
@@ -57,14 +53,17 @@ public static class GrpcServiceJsonExtensions
     }
 
     /// <summary>
-    /// Attempts to deserialize a JSON string to a GrpcService instance
+    /// Attempts to deserialize a JSON string to a <see cref="GrpcService"/> instance.
     /// </summary>
-    /// <param name="json">The JSON string to deserialize</param>
-    /// <param name="value">Output parameter for the deserialized GrpcService</param>
-    /// <returns>True if deserialization succeeded, false otherwise</returns>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Output parameter for the deserialized <see cref="GrpcService"/>.</param>
+    /// <returns>True if deserialization succeeded, false otherwise.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/>.</exception>
     public static bool TryFromJson(string json, out GrpcService? value)
     {
         value = null;
+
+        ArgumentNullException.ThrowIfNull(json);
 
         if (string.IsNullOrWhiteSpace(json))
         {
