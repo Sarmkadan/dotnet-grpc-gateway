c55502f1fd431a7693c5cc142b168f5a93977a55
Vladyslav Zaiets <rutova2@gmail.com>
2022-05-09 17:17:49 +0300

    more RequestMetric extension methods

--- tests/dotnet-grpc-gateway.Tests/RequestMetricTestsExtensions.cs
+++ tests/dotnet-grpc-gateway.Tests/RequestMetricTestsExtensions.cs
@@ -4,23 +4,37 @@ using DotNetGrpcGateway.Domain;
 
 namespace DotNetGrpcGateway.Tests
 {
+    /// <summary>
+    /// Provides extension methods for creating test instances of <see cref="RequestMetric"/> for unit testing scenarios.
+    /// </summary>
     public static class RequestMetricTestsExtensions
     {
         /// <summary>
-        /// Creates a valid RequestMetric instance with default values for testing purposes.
+        /// Creates a valid <see cref="RequestMetric"/> instance with default values for testing purposes.
         /// This extension method provides a convenient way to create test metrics without
-        /// manually constructing RequestMetric objects in test setup.
+        /// manually constructing <see cref="RequestMetric"/> objects in test setup.
         /// </summary>
-        /// <param name="tests">The RequestMetricTests instance (unused, for extension method syntax)</param>
-        /// <param name="serviceName">Service name to set</param>
-        /// <param name="methodName">Method name to set</param>
-        /// <param name="clientIpAddress">Client IP address to set</param>
-        /// <returns>A new RequestMetric instance ready for testing</returns>
-        public static RequestMetric CreateValidMetric(this RequestMetricTests tests,
+        /// <param name="tests">The <see cref="RequestMetricTests"/> instance (unused, for extension method syntax).</param>
+        /// <param name="serviceName">Service name to set. Cannot be null or empty.</param>
+        /// <param name="methodName">Method name to set. Cannot be null or empty.</param>
+        /// <param name="clientIpAddress">Client IP address to set. Cannot be null or empty.</param>
+        /// <returns>A new <see cref="RequestMetric"/> instance ready for testing.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="serviceName"/>, <paramref name="methodName"/>, or <paramref name="clientIpAddress"/> is null.</exception>
+        /// <exception cref="ArgumentException">Thrown if <paramref name="serviceName"/>, <paramref name="methodName"/>, or <paramref name="clientIpAddress"/> is empty.</exception>
+        public static RequestMetric CreateValidMetric(
+            this RequestMetricTests tests,
             string serviceName = "TestService",
             string methodName = "TestMethod",
             string clientIpAddress = "192.168.1.100")
         {
+            ArgumentNullException.ThrowIfNull(serviceName);
+            ArgumentNullException.ThrowIfNull(methodName);
+            ArgumentNullException.ThrowIfNull(clientIpAddress);
+
+            ArgumentException.ThrowIfNullOrEmpty(serviceName);
+            ArgumentException.ThrowIfNullOrEmpty(methodName);
+            ArgumentException.ThrowIfNullOrEmpty(clientIpAddress);
+
             return new RequestMetric
             {
                 ServiceName = serviceName,
@@ -35,11 +49,11 @@ namespace DotNetGrpcGateway.Tests
         }
 
         /// <summary>
-        /// Creates an invalid RequestMetric instance with empty service name for negative testing.
+        /// Creates an invalid <see cref="RequestMetric"/> instance with empty service name for negative testing.
         /// Useful for testing validation error handling scenarios.
         /// </summary>
-        /// <param name="tests">The RequestMetricTests instance (unused, for extension method syntax)</param>
-        /// <returns>A new RequestMetric instance with empty service name</returns>
+        /// <param name="tests">The <see cref="RequestMetricTests"/> instance (unused, for extension method syntax).</param>
+        /// <returns>A new <see cref="RequestMetric"/> instance with empty service name.</returns>
         public static RequestMetric CreateInvalidServiceNameMetric(this RequestMetricTests tests)
         {
             return new RequestMetric
@@ -51,12 +65,12 @@ namespace DotNetGrpcGateway.Tests
         }
 
         /// <summary>
-        /// Creates a RequestMetric instance with negative duration for testing validation.
+        /// Creates a <see cref="RequestMetric"/> instance with negative duration for testing validation.
         /// Useful for testing duration validation scenarios.
         /// </summary>
-        /// <param name="tests">The RequestMetricTests instance (unused, for extension method syntax)</param>
-        /// <param name="durationMs">Negative duration value</param>
-        /// <returns>A new RequestMetric instance with negative duration</returns>
+        /// <param name="tests">The <see cref="RequestMetricTests"/> instance (unused, for extension method syntax).</param>
+        /// <param name="durationMs">Negative duration value.</param>
+        /// <returns>A new <see cref="RequestMetric"/> instance with negative duration.</returns>
         public static RequestMetric CreateNegativeDurationMetric(this RequestMetricTests tests, double durationMs = -100)
         {
             return new RequestMetric
@@ -69,19 +83,26 @@ namespace DotNetGrpcGateway.Tests
         }
 
         /// <summary>
-        /// Creates a collection of RequestMetric instances with varying durations.
+        /// Creates a collection of <see cref="RequestMetric"/> instances with varying durations.
         /// Useful for performance testing and benchmarking scenarios.
         /// </summary>
-        /// <param name="tests">The RequestMetricTests instance (unused, for extension method syntax)</param>
-        /// <param name="count">Number of metrics to create</param>
-        /// <param name="startDurationMs">Starting duration in milliseconds</param>
-        /// <param name="durationIncrementMs">Increment between each metric</param>
-        /// <returns>IEnumerable of RequestMetric instances</returns>
-        public static IEnumerable<RequestMetric> CreateDurationSequence(this RequestMetricTests tests,
+        /// <param name="tests">The <see cref="RequestMetricTests"/> instance (unused, for extension method syntax).</param>
+        /// <param name="count">Number of metrics to create. Must be non-negative.</param>
+        /// <param name="startDurationMs">Starting duration in milliseconds.</param>
+        /// <param name="durationIncrementMs">Increment between each metric.</param>
+        /// <returns>IEnumerable of <see cref="RequestMetric"/> instances.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="count"/> is negative.</exception>
+        public static IEnumerable<RequestMetric> CreateDurationSequence(
+            this RequestMetricTests tests,
             int count,
             double startDurationMs = 0,
             double durationIncrementMs = 100)
         {
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count), "Count must be non-negative.");
+            }
+
             for (int i = 0; i < count; i++)
             {
                 yield return new RequestMetric
@@ -97,12 +118,12 @@ namespace DotNetGrpcGateway.Tests
         }
 
         /// <summary>
-        /// Creates a RequestMetric instance that represents a slow request.
+        /// Creates a <see cref="RequestMetric"/> instance that represents a slow request.
         /// Useful for testing slow request detection logic.
         /// </summary>
-        /// <param name="tests">The RequestMetricTests instance (unused, for extension method syntax)</param>
-        /// <param name="thresholdMs">Threshold that defines a slow request</param>
-        /// <returns>A new RequestMetric instance with duration above threshold</returns>
+        /// <param name="tests">The <see cref="RequestMetricTests"/> instance (unused, for extension method syntax).</param>
+        /// <param name="thresholdMs">Threshold that defines a slow request.</param>
+        /// <returns>A new <see cref="RequestMetric"/> instance with duration above threshold.</returns>
         public static RequestMetric CreateSlowRequest(this RequestMetricTests tests, double thresholdMs = 1000)
         {
             return new RequestMetric
@@ -117,17 +138,23 @@ namespace DotNetGrpcGateway.Tests
         }
 
         /// <summary>
-        /// Creates a RequestMetric instance with error state for testing error handling.
+        /// Creates a <see cref="RequestMetric"/> instance with error state for testing error handling.
         /// Useful for testing error recovery and monitoring scenarios.
         /// </summary>
-        /// <param name="tests">The RequestMetricTests instance (unused, for extension method syntax)</param>
-        /// <param name="errorMessage">Error message to set</param>
-        /// <param name="includeStackTrace">Whether to include stack trace</param>
-        /// <returns>A new RequestMetric instance with error state</returns>
-        public static RequestMetric CreateErrorMetric(this RequestMetricTests tests,
+        /// <param name="tests">The <see cref="RequestMetricTests"/> instance (unused, for extension method syntax).</param>
+        /// <param name="errorMessage">Error message to set. Cannot be null or empty.</param>
+        /// <param name="includeStackTrace">Whether to include stack trace.</param>
+        /// <returns>A new <see cref="RequestMetric"/> instance with error state.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="errorMessage"/> is null.</exception>
+        /// <exception cref="ArgumentException">Thrown if <paramref name="errorMessage"/> is empty.</exception>
+        public static RequestMetric CreateErrorMetric(
+            this RequestMetricTests tests,
             string errorMessage = "Connection timeout",
             bool includeStackTrace = true)
         {
+            ArgumentNullException.ThrowIfNull(errorMessage);
+            ArgumentException.ThrowIfNullOrEmpty(errorMessage);
+
             var metric = new RequestMetric
             {
                 ServiceName = "ErrorService",
@@ -152,14 +179,20 @@ namespace DotNetGrpcGateway.Tests
         }
 
         /// <summary>
-        /// Creates a RequestMetric instance with retry tracking enabled.
+        /// Creates a <see cref="RequestMetric"/> instance with retry tracking enabled.
         /// Useful for testing retry logic and circuit breaker patterns.
         /// </summary>
-        /// <param name="tests">The RequestMetricTests instance (unused, for extension method syntax)</param>
-        /// <param name="retryCount">Number of retries to simulate</param>
-        /// <returns>A new RequestMetric instance with retry tracking</returns>
+        /// <param name="tests">The <see cref="RequestMetricTests"/> instance (unused, for extension method syntax).</param>
+        /// <param name="retryCount">Number of retries to simulate. Must be non-negative.</param>
+        /// <returns>A new <see cref="RequestMetric"/> instance with retry tracking.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="retryCount"/> is negative.</exception>
         public static RequestMetric CreateRetryMetric(this RequestMetricTests tests, int retryCount = 3)
         {
+            if (retryCount < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(retryCount), "Retry count must be non-negative.");
+            }
+
             var metric = new RequestMetric
             {
                 ServiceName = "RetryService",
