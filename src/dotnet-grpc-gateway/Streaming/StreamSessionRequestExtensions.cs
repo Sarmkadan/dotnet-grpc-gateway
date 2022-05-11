a4e795d9c9d5e5cc8f0922beb339d8b7a6665d48
Vladyslav Zaiets <rutova2@gmail.com>
2022-05-11 19:45:45 +0300

    feat(streaming): add extension helpers for StreamSessionRequest

---
+++ src/dotnet-grpc-gateway/Streaming/StreamSessionRequestExtensions.cs
@@ -0,0 +1,61 @@
+#nullable enable
+// =============================================================================
+// Author: [Your Name]
+// =============================================================================
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using DotNetGrpcGateway.Streaming;
+
+namespace DotNetGrpcGateway.Streaming;
+
+/// <summary>
+/// Extension methods for <see cref="StreamSessionRequest"/> to enhance request processing and analysis.
+/// </summary>
+public static class StreamSessionRequestExtensions
+{
+    /// <summary>
+    /// Determines whether the request contains a valid service and method name.
+    /// </summary>
+    /// <param name="request">The request to check.</param>
+    /// <returns><see langword="true"/> if the request has a valid service and method name; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="request"/> is <see langword="null"/>.</exception>
+    public static bool HasValidServiceAndMethod(this StreamSessionRequest request)
+    {
+        ArgumentNullException.ThrowIfNull(request);
+        return !string.IsNullOrEmpty(request.ServiceName) && !string.IsNullOrEmpty(request.MethodName);
+    }
+
+    /// <summary>
+    /// Gets a read-only dictionary of headers from the request, excluding any null or empty keys.
+    /// </summary>
+    /// <param name="request">The request to retrieve headers from.</param>
+    /// <returns>A read-only dictionary of valid headers.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="request"/> is <see langword="null"/>.</exception>
+    public static IReadOnlyDictionary<string, string> GetValidHeaders(this StreamSessionRequest request)
+    {
+        ArgumentNullException.ThrowIfNull(request);
+        var validHeaders = new Dictionary<string, string>();
+        foreach (var header in request.Headers)
+        {
+            if (!string.IsNullOrEmpty(header.Key))
+            {
+                validHeaders[header.Key] = header.Value;
+            }
+        }
+        return validHeaders;
+    }
+
+    /// <summary>
+    /// Creates a human-readable summary of the request for logging or auditing purposes.
+    /// </summary>
+    /// <param name="request">The request to summarize.</param>
+    /// <returns>A summary string containing service, method, route path, and header count.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="request"/> is <see langword="null"/>.</exception>
+    public static string ToRequestSummary(this StreamSessionRequest request)
+    {
+        ArgumentNullException.ThrowIfNull(request);
+        return $"Service: {request.ServiceName}, Method: {request.MethodName}, Route: {request.RoutePath ?? "default"}, Headers: {request.Headers.Count}";
+    }
+}
