6d1d25f39b8b4b63609a592d1e1fd42cf897f8c6
Vladyslav Zaiets <rutova2@gmail.com>
2022-05-13 20:27:52 +0300

    feat(controllers): json round-trip support for ReflectionControllerJson

--- src/dotnet-grpc-gateway/Controllers/ReflectionControllerJsonExtensions.cs
+++ src/dotnet-grpc-gateway/Controllers/ReflectionControllerJsonExtensions.cs
@@ -1,73 +1,71 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
-namespace DotNetGrpcGateway.Controllers;
-
 /// <summary>
 /// Provides extension methods for serializing and deserializing <see cref="ReflectionController"/> instances to and from JSON.
 /// </summary>
 public static class ReflectionControllerJsonExtensions
 {
-	private static readonly JsonSerializerOptions _jsonOptions = new()
-	{
-		PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-		WriteIndented = false,
-		DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
-	};
+    private static readonly JsonSerializerOptions _jsonOptions = new()
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+    };
 
-	/// <summary>
-	/// Serializes a <see cref="ReflectionController"/> instance to a JSON string.
-	/// </summary>
-	/// <param name="value">The controller instance to serialize.</param>
-	/// <param name="indented">Whether to format the JSON with indentation for readability.</param>
-	/// <returns>A JSON string representation of the controller.</returns>
-	/// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
-	public static string ToJson(this ReflectionController value, bool indented = false)
-	{
-		ArgumentNullException.ThrowIfNull(value);
+    /// <summary>
+    /// Serializes a <see cref="ReflectionController"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The controller instance to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the controller.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    public static string ToJson(this ReflectionController value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
 
-		var options = indented
-			? new JsonSerializerOptions(_jsonOptions)
-			{
-				WriteIndented = true
-			} : _jsonOptions;
+        var options = indented
+            ? new JsonSerializerOptions(_jsonOptions)
+            {
+                WriteIndented = true
+            } : _jsonOptions;
 
-		return JsonSerializer.Serialize(value, options);
-	}
+        return JsonSerializer.Serialize(value, options);
+    }
 
-	/// <summary>
-	/// Deserializes a JSON string to a <see cref="ReflectionController"/> instance.
-	/// </summary>
-	/// <param name="json">The JSON string to deserialize.</param>
-	/// <returns>The deserialized controller instance, or null if deserialization fails.</returns>
-	/// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
-	public static ReflectionController? FromJson(string json)
-	{
-		ArgumentException.ThrowIfNullOrEmpty(json);
+    /// <summary>
+    /// Deserializes a JSON string to a <see cref="ReflectionController"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized controller instance, or null if deserialization fails.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null or empty.</exception>
+    public static ReflectionController? FromJson(string json)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
 
-		return JsonSerializer.Deserialize<ReflectionController>(json, _jsonOptions);
-	}
+        return JsonSerializer.Deserialize<ReflectionController>(json, _jsonOptions);
+    }
 
-	/// <summary>
-	/// Attempts to deserialize a JSON string to a <see cref="ReflectionController"/> instance.
-	/// </summary>
-	/// <param name="json">The JSON string to deserialize.</param>
-	/// <param name="value">Receives the deserialized controller instance if successful.</param>
-	/// <returns>True if deserialization succeeds; otherwise, false.</returns>
-	/// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
-	public static bool TryFromJson(string json, out ReflectionController? value)
-	{
-		ArgumentException.ThrowIfNullOrEmpty(json);
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a <see cref="ReflectionController"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized controller instance if successful.</param>
+    /// <returns>True if deserialization succeeds; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null or empty.</exception>
+    public static bool TryFromJson(string json, out ReflectionController? value)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
 
-		try
-		{
-			value = JsonSerializer.Deserialize<ReflectionController>(json, _jsonOptions);
-			return true;
-		}
-		catch (JsonException)
-		{
-			value = null;
-			return false;
-		}
-	}
+        try
+        {
+            value = JsonSerializer.Deserialize<ReflectionController>(json, _jsonOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            value = null;
+            return false;
+        }
+    }
 }
