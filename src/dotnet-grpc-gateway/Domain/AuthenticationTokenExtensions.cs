7898acc926cd329c299d4e4c80e5f29df65c19df
Vladyslav Zaiets <rutova2@gmail.com>
2022-05-13 19:21:14 +0300

    feat(domain): AuthenticationToken extensions

--- src/dotnet-grpc-gateway/Domain/AuthenticationTokenExtensions.cs
+++ src/dotnet-grpc-gateway/Domain/AuthenticationTokenExtensions.cs
@@ -10,12 +10,11 @@ namespace DotNetGrpcGateway.Domain
         /// <summary>
         /// Determines whether the token contains the specified scope.
         /// </summary>
-        /// <param name="token">The authentication token to check. Cannot be null.</param>
-        /// <param name="scope">The scope to search for. Cannot be null or empty.</param>
-        /// <returns><see langword="true"/> if the token contains the scope; otherwise, <see langword="false"/>.</returns>
-        /// <exception cref="ArgumentNullException"><paramref name="token"/> is <see langword="null"/>.</exception>
-        /// <exception cref="ArgumentNullException"><paramref name="scope"/> is <see langword="null"/>.</exception>
-        /// <exception cref="ArgumentException"><paramref name="scope"/> is empty or whitespace.</exception>
+        /// <param name="token">The authentication token to check. Cannot be <c>null</c>.</param>
+        /// <param name="scope">The scope to search for. Cannot be <c>null</c>, empty, or whitespace.</param>
+        /// <returns><see langword="true"/> if the token's <c>Scopes</c> collection contains <paramref name="scope"/> (case‑insensitive); otherwise, <see langword="false"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="token"/> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentException"><paramref name="scope"/> is <c>null</c>, empty, or consists only of whitespace.</exception>
         public static bool HasScope(this AuthenticationToken token, string scope)
         {
             ArgumentNullException.ThrowIfNull(token);
@@ -27,9 +26,12 @@ namespace DotNetGrpcGateway.Domain
         /// <summary>
         /// Determines whether the token has expired.
         /// </summary>
-        /// <param name="token">The authentication token to check. Cannot be null.</param>
-        /// <returns><see langword="true"/> if the token has expired; otherwise, <see langword="false"/>.</returns>
-        /// <exception cref="ArgumentNullException"><paramref name="token"/> is <see langword="null"/>.</exception>
+        /// <param name="token">The authentication token to check. Cannot be <c>null</c>.</param>
+        /// <returns>
+        /// <see langword="true"/> if the token defines an <c>ExpiresAt</c> value and that value is earlier than the current UTC time;
+        /// otherwise, <see langword="false"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException"><paramref name="token"/> is <c>null</c>.</exception>
         public static bool IsExpired(this AuthenticationToken token)
         {
             ArgumentNullException.ThrowIfNull(token);
@@ -40,10 +42,15 @@ namespace DotNetGrpcGateway.Domain
         /// <summary>
         /// Determines whether the token is near expiry based on the provided threshold.
         /// </summary>
-        /// <param name="token">The authentication token to check. Cannot be null.</param>
-        /// <param name="threshold">The time span before expiry to consider the token as near expiry. Must be non-negative.</param>
-        /// <returns><see langword="true"/> if the token is near expiry; otherwise, <see langword="false"/>.</returns>
-        /// <exception cref="ArgumentNullException"><paramref name="token"/> is <see langword="null"/>.</exception>
+        /// <param name="token">The authentication token to check. Cannot be <c>null</c>.</param>
+        /// <param name="threshold">
+        /// The time span before expiry that qualifies the token as “near expiry”. Must be zero or positive.
+        /// </param>
+        /// <returns>
+        /// <see langword="true"/> if the token defines an <c>ExpiresAt</c> value and the remaining time until that expiry
+        /// is less than or equal to <paramref name="threshold"/>; otherwise, <see langword="false"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException"><paramref name="token"/> is <c>null</c>.</exception>
         /// <exception cref="ArgumentOutOfRangeException"><paramref name="threshold"/> is negative.</exception>
         public static bool IsNearExpiry(this AuthenticationToken token, TimeSpan threshold)
         {
