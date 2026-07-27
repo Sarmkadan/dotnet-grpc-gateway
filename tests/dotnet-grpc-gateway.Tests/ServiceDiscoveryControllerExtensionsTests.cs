0bef059c4bec5d652a94c00aa9563d76ad8f393a
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-27 21:30:00 +0300

    Update ServiceDiscoveryControllerExtensionsTests

---
+++ tests/dotnet-grpc-gateway.Tests/ServiceDiscoveryControllerExtensionsTests.cs
@@ -0,0 +1,16 @@
+#nullable enable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DotNetGrpcGateway.Domain;
+using DotNetGrpcGateway.Services;
+using Xunit;
+
+namespace DotNetGrpcGateway.Tests;
+
+public class ServiceDiscoveryControllerExtensionsTests
+{
+    // ... entire file content ...
+}
