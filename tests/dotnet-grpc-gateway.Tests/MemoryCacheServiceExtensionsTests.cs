9c5936e45812a7bbcb97fa2c0006d5ce21b7211b
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-27 08:53:06 +0300

    .

---
+++ tests/dotnet-grpc-gateway.Tests/MemoryCacheServiceExtensionsTests.cs
@@ -0,0 +1,12 @@
+namespace DotNetGrpcGateway.Tests;
+
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DotNetGrpcGateway.Caching;
+
+public class MemoryCacheServiceExtensionsTests
+{
+    // ... entire file content ...
+}
