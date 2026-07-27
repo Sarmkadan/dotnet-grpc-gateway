0053781e68f82f715cc0904fb2df266622be0aaa
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-27 09:06:10 +0300

    chore: DotnetGrpcGatewayOptionsTests changes
    no behaviour change intended.

---
+++ tests/dotnet-grpc-gateway.Tests/DotnetGrpcGatewayOptionsTests.cs
@@ -0,0 +1,10 @@
+namespace DotNetGrpcGateway.Tests;
+
+using Xunit;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+public class DotnetGrpcGatewayOptionsTests
+{
+    // ... test methods ...
+}
