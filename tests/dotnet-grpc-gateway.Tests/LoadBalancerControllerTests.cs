c1d37732c5f282a02aec372e88101272203f4949
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-28 07:27:17 +0300

    tweak LoadBalancerControllerTests

---
+++ tests/dotnet-grpc-gateway.Tests/LoadBalancerControllerTests.cs
@@ -0,0 +1,16 @@
+#nullable enable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+using DotNetGrpcGateway.Controllers;
+using DotNetGrpcGateway.Domain;
+using DotNetGrpcGateway.Services;
+
+namespace DotNetGrpcGateway.Tests;
+
+public class LoadBalancerControllerTests
+{
+    // ... (rest of the file remains the same)
+}
