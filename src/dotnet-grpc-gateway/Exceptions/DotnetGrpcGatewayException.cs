3a11e5bf0a51f975eaeb25c026b28618c950100d
Vladyslav Zaiets <rutova2@gmail.com>
2026-09-12 05:55:19 +0000

    chore: maintenance updates

--- src/dotnet-grpc-gateway/Exceptions/DotnetGrpcGatewayException.cs
+++ src/dotnet-grpc-gateway/Exceptions/DotnetGrpcGatewayException.cs
@@ -8,9 +8,22 @@ namespace DotNetGrpcGateway.Exceptions;
 /// </summary>
 public class DotnetGrpcGatewayException : Exception
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DotnetGrpcGatewayException"/> class.
+    /// </summary>
     public DotnetGrpcGatewayException() { }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DotnetGrpcGatewayException"/> class with a specified error message.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
     public DotnetGrpcGatewayException(string message) : base(message) { }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DotnetGrpcGatewayException"/> class with a specified error message
+    /// and a reference to the inner exception that is the cause of this exception.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
+    /// <param name="innerException">The exception that is the cause of the current exception.</param>
     public DotnetGrpcGatewayException(string message, Exception innerException) : base(message, innerException) { }
 }
